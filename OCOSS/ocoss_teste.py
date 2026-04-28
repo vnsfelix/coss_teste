3836ab9dab755858bf1cb2e5b4cac0331d20181b
Felix <vfelix2022santos@gmail.com>
2026-04-27 23:54:08 -0300

    feat: spotify

--- OCOSS/ocoss_teste.py
+++ OCOSS/ocoss_teste.py
@@ -2,4 +2,8 @@ import os
 
 def iniciar_ocoss():
     while True:
-        comando = input("O que o senhor gostaria de fazer hoje? (Ou digite 'sair': )").lower()
+        comando = input("O que o senhor gostaria de fazer hoje? (Ou digite 'sair'): ").lower()
+
+        if "spotify" in comando:
+            print("Deseja abir no [1] Aplicativo ou [2] Navegador?")
+            escolha = input("> ")
