d247d96861eb00a5960e47a4570e21a1df51143c
Vinicius Felix <vfelix2022santos@gmail.com>
2026-04-29 16:08:02 -0300

    fix commands 2.0

--- OCOSS/ocoss_teste.py
+++ OCOSS/ocoss_teste.py
@@ -10,16 +10,13 @@ def iniciar_ocoss():
 
             if escolha == "1":
                 print("Abrindo Previsão do Tempo...")
-                os.system("start bingweather:")
-                
+                os.system("start bingweather:")    
             elif escolha == "2":
                 print("Abrindo Navegador")
                 os.system("start https://www.google.com/search?q=google+clima&sca_esv=09c971a9c1a6e973&biw=1920&bih=945&sxsrf=ANbL-n7cpq_tZ9DNPy_0SitfpngNnj761g%3A1777471634152&ei=khDyafGFCdCf5OUP2OujqQ4&oq=goo&gs_lp=Egxnd3Mtd2l6LXNlcnAiA2dvbyoCCAEyFhAuGIAEGIoFGEMYsQMYgwEYxwEY0QMyChAAGIAEGIoFGEMyChAAGIAEGIoFGEMyDRAAGIAEGIoFGEMYsQMyCxAAGIAEGLEDGIMBMhAQABiABBiKBRhDGLEDGIMBMggQABiABBixAzIQEAAYgAQYigUYQxixAxiDATINEAAYgAQYigUYQxixAzILEAAYgAQYsQMYgwEyJRAuGIAEGIoFGEMYsQMYgwEYxwEY0QMYlwUY3AQY3gQY4ATYAQJIjhdQ7wNY3w5wAngBkAEDmAFxoAHnCqoBBDIuMTG4AQPIAQD4AQGYAgagAqsDqAIUwgIKEAAYRxjWBBiwA8ICDRAAGIAEGIoFGEMYsAPCAhcQLhjYAhi4BhjaBhjcBhjIAxiwA9gBAcICFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBwgIQEAAYgAQYigUYQxixAxjJA8ICCxAAGIAEGLEDGJIDwgILEAAYgAQYigUYkgPCAgcQABiABBgKwgIFEAAYgATCAhYQABiABBiKBRhDGOcGGOoCGLQC2AEBwgIcEC4YgAQYigUYQxjnBhjHARjRAxjqAhi0AtgBAcICEBAAGAMYjwEY6gIYtALYAQLCAhAQLhgDGI8BGOoCGLQC2AECwgIREC4YgAQYsQMYgwEYxwEY0QPCAhMQLhiABBiKBRhDGLEDGMcBGNEDmAME8QVVAT6-PZhd6ogGAZAGDboGBAgBGBm6BgYIAhABGAqSBwMyLjSgB8ZPsgcDMC40uAejA8IHBTAuMy4zyAcRgAgB&sclient=gws-wiz-serp")
             elif escolha == "3":
                 print("Voltando...")
                 continue
-            else:
-                print("opção invalida.")
 
         if "calculadora" in comando:
             print("Deseja abir no [1] Aplicativo ou [2] voltar?")
@@ -31,10 +28,6 @@ def iniciar_ocoss():
             elif escolha == "2":
                 print("Voltando...")
                 continue
-            else:
-                print("opção invalida.")
-        else:
-            print("Desculpe, não reconheço esse comando.")
 
         if "spotify" in comando:
             print("Deseja abir no [1] Aplicativo ou [2] Navegador?")
@@ -46,9 +39,20 @@ def iniciar_ocoss():
             elif escolha == "2":
                 print("Abrindo Navegador...")
                 os.system("start https://www.spotify.com")
-            else:
-                print("opção invalida.")
+
+        if "youtube" in comando:
+            print("Digite [1] para navegador ou [2] para voltar ")
+            escolha = input("> ")
+
+            if escolha == "1":
+                print("Abrindo Navegador...")
+                os.system("start https://www.youtube.com") 
+            elif escolha == "2":
+                print("Voltando...")
+        elif "sair" in comando:
+            print("Finalizando Sistema...")
+            break
         else:
             print("Desculpe, não reconheço esse comando.")
-        
+
 iniciar_ocoss()
