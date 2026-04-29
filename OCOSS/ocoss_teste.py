48a600032bac784a451893c5c0fc6a3d1a579fba
Vinicius Felix <vfelix2022santos@gmail.com>
2026-04-29 07:57:58 -0300

    Add calculator and Spotify command options

--- OCOSS/ocoss_teste.py
+++ OCOSS/ocoss_teste.py
@@ -4,6 +4,21 @@ def iniciar_ocoss():
     while True:
         comando = input("\nO que o senhor gostaria de fazer hoje? (Ou digite 'sair'): ").lower().strip()
 
+        if "calculadora" in comando:
+            print("Deseja abir o [1] Aplicativo? ou [2] para sair")
+            escolha = input("> ")
+
+            if escolha == "1":
+                print("Abrindo calculadora...")
+                os.system("start Calculadora")
+            else:
+                print("opção invalida.")
+        elif escolha == "2":
+            print("Finalizando Sistema...")
+            break
+        else:
+            print("Desculpe, não reconheço esse comando.")
+
         if "spotify" in comando:
             print("Deseja abir no [1] Aplicativo ou [2] Navegador?")
             escolha = input("> ")
